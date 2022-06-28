73354cb7e074fd37278c056b074780821f166c9d
Laura Findley <laura@volanto.com>
2022-06-28 21:22:06 +0100

    Add interface for TrainingImageRepository

--- blueprints/training_image/data/repository.py
+++ blueprints/training_image/data/repository.py
@@ -1,8 +1,23 @@
 import pymongo
+from ..model import TrainingImage, TrainingImageLabel
 
 class TrainingImageRepository:
     def __init__(self):
         pass
 
-    def store(self, image):
+    def create(self, image):
+        if not isinstance(image, TrainingImage):
+            raise Exception("The image to store must be a valid TrainingImage instance")
+        
+
+    def update(self, image):
+        pass
+
+    def get(self, id):
+        pass
+
+    def list(self):
+        pass
+
+    def __serialise_image(self, object):
         pass
