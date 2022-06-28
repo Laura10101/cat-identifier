0e3fe66ad6a4c07e4c358b60360fbfe6aabe6c56
Laura Findley <laura@volanto.com>
2022-06-28 21:32:23 +0100

    Add serialisation methods to TrainingImageRepo

--- blueprints/training_image/data/repository.py
+++ blueprints/training_image/data/repository.py
@@ -9,7 +9,6 @@ class TrainingImageRepository:
         if not isinstance(image, TrainingImage):
             raise Exception("The image to store must be a valid TrainingImage instance")
         
-
     def update(self, image):
         pass
 
@@ -19,5 +18,19 @@ class TrainingImageRepository:
     def list(self):
         pass
 
-    def __serialise_image(self, object):
-        pass
+    def __serialise_image(self, image):
+        return {
+            "image": image.get_image(),
+            "source": image.get_source(),
+            "label": self.__serialise_image_label(self, image.get_label()),
+            "is_labelled": image.get_is_labelled()
+        }
+
+    def __serialise_image_label(self, label):
+        return {
+            "is_cat": label.get_is_cat(),
+            "colour": label.get_colour(),
+            "is_tabby": label.get_is_tabby(),
+            "pattern": label.get_pattern(),
+            "is_pointed": label.get_is_pointed()
+        }
