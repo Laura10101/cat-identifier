e4f83fac635ea71ddc056dc8e6947b42de9ab793
Laura Findley <laura@volanto.com>
2022-07-01 16:01:34 +0100

    Implement create training image service

--- blueprints/training_image/services/training_image_service.py
+++ blueprints/training_image/services/training_image_service.py
@@ -1,6 +1,11 @@
+from ..model import TrainingImage, TrainingImageLabel
+from ..data import TrainingImageRepository
+
 class TrainingImageService:
     def __init__(self):
         pass
 
-    def create_training_image(self, training_image_data):
-        pass
+    def create_training_image(self, image):
+        image = TrainingImage(image=image, source='Admin', label=TrainingImageLabel(), is_labelled=False)
+        repo = TrainingImageRepository()
+        return repo.create_one(image)
