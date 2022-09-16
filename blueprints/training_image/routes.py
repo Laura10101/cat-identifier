0205f53ebf584c468a39fbd010f6df775cb77100
Laura Findley <laura@volanto.com>
2022-09-16 19:49:18 +0100

    Correctly parse HTTP request data types

--- blueprints/training_image/routes.py
+++ blueprints/training_image/routes.py
@@ -12,6 +12,7 @@ training_image_bp = Blueprint(
 )
 
 UPLOAD_PATH = "/tmp/uploads/"
+TRUE = "true"
 
 @training_image_bp.route('/', methods=['POST'])
 def post_training_image():
@@ -39,17 +40,17 @@ def get_unlabelled_training_images():
 
 #routing for the set image label service
 @training_image_bp.route('/<id>/label', methods=['POST'])
-def set_image_label():
+def set_image_label(id):
     #set up try/except clauses to handle happy path and exceptional paths
     try: 
         service = TrainingImageService() 
-        is_cat = request.form.get("is_cat")
+        is_cat = request.form.get("is_cat") == TRUE
         colour = request.form.get("colour")
-        is_tabby = request.form.get("is_tabby")
+        is_tabby = request.form.get("is_tabby") == TRUE
         pattern = request.form.get("pattern")
-        is_pointed = request.form.get("is_pointed")
+        is_pointed = request.form.get("is_pointed") == TRUE
         service.set_image_label(id, is_cat, colour, is_tabby, pattern, is_pointed)
-        
+        return {}, 200
     except Exception as e:
         return { "error": str(e) }, 400
 
