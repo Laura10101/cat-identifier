11b93d1d38acf88d3cabd94acf11098f0df6b36d
Laura Findley <laura@volanto.com>
2022-09-16 19:30:48 +0100

    Add set image label route and make route URLs more RESTful

--- blueprints/training_image/routes.py
+++ blueprints/training_image/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, current_app as app
 from .services import TrainingImageService
+from base64 import b64encode
 from werkzeug.utils import secure_filename
 import os
 
@@ -21,18 +22,34 @@ def post_training_image():
         if image.filename == "" or not image or not is_allowed_extension(image.filename):
             raise Exception("A valid .png or .jpg image must be provided when posting a training image")
 
-        image_id = service.create_training_image(image_file=image)
+        image_id = service.create_training_image(image_file=b64encode(image.read()))
         return { "id" : image_id }, 200
     except Exception as e:
         return { "error": str(e) }, 400
 
-### GET UNLABELLED TRAINING IMAGES ###
-@training_image_bp.route('/', methods=['GET'])
+#GET UNLABELLED TRAINING IMAGES
+@training_image_bp.route('/unlabelled', methods=['GET'])
 def get_unlabelled_training_images():
     try:
         service = TrainingImageService()
         images = [image.serialize() for image in service.get_unlabelled_images()]
         return { "images": images }, 200
+    except Exception as e:
+        return { "error": str(e) }, 400
+
+#routing for the set image label service
+@training_image_bp.route('/<id>/label', methods=['POST'])
+def set_image_label():
+    #set up try/except clauses to handle happy path and exceptional paths
+    try: 
+        service = TrainingImageService() 
+        is_cat = request.form.get("is_cat")
+        colour = request.form.get("colour")
+        is_tabby = request.form.get("is_tabby")
+        pattern = request.form.get("pattern")
+        is_pointed = request.form.get("is_pointed")
+        service.set_image_label(id, is_cat, colour, is_tabby, pattern, is_pointed)
+        
     except Exception as e:
         return { "error": str(e) }, 400
 
