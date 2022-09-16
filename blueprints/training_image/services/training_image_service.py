0bdcd0b8f8f8cb42a2fa4bc40d47ba621e6a3320
Laura Findley <laura@volanto.com>
2022-09-16 12:19:57 +0100

    Implement service method to fetch unlabelled images

--- blueprints/training_image/services/training_image_service.py
+++ blueprints/training_image/services/training_image_service.py
@@ -5,7 +5,19 @@ class TrainingImageService:
     def __init__(self):
         pass
 
-    def create_training_image(self, image):
-        image = TrainingImage(image=image, source='Admin', label=TrainingImageLabel(), is_labelled=False)
+    def create_training_image(self, image_file):
+        image = TrainingImage(source='Admin', label=TrainingImageLabel(), is_labelled=False)
         repo = TrainingImageRepository()
-        return repo.create_one(image)
+        return repo.create_one(image, image_file)
+
+    #create service layer function to retrieve images which have not yet been labelled
+    def get_unlabelled_images(self):
+        #create a new image of the training image repository class and assign it to variable 'repo'
+        #this allows access to the functions in the data layer
+        repo = TrainingImageRepository()
+        #call data layer function get_unlabelled_images and return the result 
+        return repo.get_unlabelled_images()
+        
+    #create method to update the label of an unlabelled image 
+    def set_image_label(self, id, label):
+        pass
