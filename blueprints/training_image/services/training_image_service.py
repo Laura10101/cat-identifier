0ed45d19fb0c34546b66032b0bf7cde705f73888
Laura Findley <laura@volanto.com>
2022-09-16 19:34:23 +0100

    Implement the set image label method on the training image service layer

--- blueprints/training_image/services/training_image_service.py
+++ blueprints/training_image/services/training_image_service.py
@@ -3,21 +3,24 @@ from ..data import TrainingImageRepository
 
 class TrainingImageService:
     def __init__(self):
-        pass
+        #create an instance of the training image repository that can be used by all methods of the training image service
+        self.__repo = TrainingImageRepository()
 
     def create_training_image(self, image_file):
         image = TrainingImage(source='Admin', label=TrainingImageLabel(), is_labelled=False)
-        repo = TrainingImageRepository()
-        return repo.create_one(image, image_file)
+        return self.__repo.create_one(image, image_file)
 
     #create service layer function to retrieve images which have not yet been labelled
     def get_unlabelled_images(self):
-        #create a new image of the training image repository class and assign it to variable 'repo'
         #this allows access to the functions in the data layer
-        repo = TrainingImageRepository()
         #call data layer function get_unlabelled_images and return the result 
-        return repo.get_unlabelled_images()
+        return self.__repo.get_unlabelled_images()
         
     #create method to update the label of an unlabelled image 
-    def set_image_label(self, id, label):
-        pass
+    def set_image_label(self, id, is_cat = False, colour = None, is_tabby = False, pattern = None, is_pointed=False):
+        #create an instance of the training image label model class 
+        label = TrainingImageLabel(is_cat, colour, is_tabby, pattern, is_pointed)
+        #call the update label method on the training image repository 
+        self.__repo.set_image_label(id, label)
+
+    
