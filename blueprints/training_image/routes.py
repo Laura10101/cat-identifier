b46f1946881880b0780e29e424000289b7edb0b5
Laura Findley <laura@volanto.com>
2022-09-16 12:20:52 +0100

    Fix bug in posting training image and add route to get unlabelled images

--- blueprints/training_image/routes.py
+++ blueprints/training_image/routes.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, request, current_app as app
 from .services import TrainingImageService
+from werkzeug.utils import secure_filename
+import os
 
 
 #Blueprint Configuration
@@ -8,14 +10,34 @@ training_image_bp = Blueprint(
     __name__
 )
 
+UPLOAD_PATH = "/tmp/uploads/"
 
 @training_image_bp.route('/', methods=['POST'])
 def post_training_image():
     try:
         service  = TrainingImageService()
         image = request.files["image"]
-        id = service.create_training_image(image=image)
-        return { "id" : id }, 200
+
+        if image.filename == "" or not image or not is_allowed_extension(image.filename):
+            raise Exception("A valid .png or .jpg image must be provided when posting a training image")
+
+        image_id = service.create_training_image(image_file=image)
+        return { "id" : image_id }, 200
     except Exception as e:
         return { "error": str(e) }, 400
 
+### GET UNLABELLED TRAINING IMAGES ###
+@training_image_bp.route('/', methods=['GET'])
+def get_unlabelled_training_images():
+    try:
+        service = TrainingImageService()
+        images = [image.serialize() for image in service.get_unlabelled_images()]
+        return { "images": images }, 200
+    except Exception as e:
+        return { "error": str(e) }, 400
+
+### HELPER FUNCTIONS ###
+
+def is_allowed_extension(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in { "png", "jpg" }
