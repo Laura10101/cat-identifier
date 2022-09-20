7f0204c0e23468ed57c1c6fee241f2f03e133110
Laura Findley <laura@volanto.com>
2022-09-20 21:53:11 +0100

    Implement routing for authentication, login and registration services

--- blueprints/apis/user/routes.py
+++ blueprints/apis/user/routes.py
@@ -1,7 +1,46 @@
 from flask import Blueprint, request, current_app as app
+from werkzeug.security import generate_password_hash
+from .services import UserService
 
 #Blueprint Configuration
 user_bp = Blueprint(
     'user_bp',
     __name__
 )
+
+@user_bp.route('/login', methods=['POST'])
+def login():
+    try:
+        username = request.json["username"]
+        password = request.json["password"]
+        service = UserService()
+        token = service.login(username, password)
+        if token == None:
+            return { "error": "Unrecognised username and/or password" }, 401
+        return { "token": token }, 200
+    except Exception as e:
+        return { "error": "Unrecognised username and/or password"}, 401
+
+@user_bp.route('/authenticate', methods=['POST'])
+def authenticate():
+    try:
+        username = request.json["username"]
+        token = request.json["token"]
+        service = UserService()
+        token = service.authenticate(username, token)
+        if token == None:
+            return { "error": "User authentication failed"}, 401
+        return { "token": token }, 200
+    except Exception as e:
+        return { "error": "User authentication failed"}, 401
+
+@user_bp.route('/', methods=['POST'])
+def register():
+    try:
+        username = request.json["username"]
+        password = generate_password_hash(request.json["password"])
+        service = UserService()
+        service.register_user(username, password)
+        return {}, 200
+    except Exception as e:
+        return { "error": str(e) }, 400
