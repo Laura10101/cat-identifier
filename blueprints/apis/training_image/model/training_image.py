d0bb13e4faa8279f80b3ed9035a447c89329ff55
Laura Findley <laura@volanto.com>
2022-09-22 16:20:09 +0100

    Preprocess training images for training

--- blueprints/apis/training_image/model/training_image.py
+++ blueprints/apis/training_image/model/training_image.py
@@ -1,4 +1,9 @@
 from .training_image_label import TrainingImageLabel
+from base64 import b64encode, b64decode
+import io
+from PIL import Image
+import numpy as np
+
 
 class TrainingImage:
     def __init__(self, id = None, image = None, source = "Admin", label = None, is_labelled = False):
@@ -28,6 +33,46 @@ class TrainingImage:
 
     def set_image(self, image):
         self._image = image
+
+    #create method to return pre-processed image (chains other methods together)
+    def get_preprocessed_image(self):
+        #get resized image data
+        image_data = self.__get_resized_image()
+        #convert image data to numerical data 
+        image_data = self.__get_numerical_image(image_data)
+        #return standardised image
+        return image_data
+
+    #create method to standardise image size
+    #this code was taken from stack overflow, see link below
+    #https://stackoverflow.com/questions/61574724/how-to-resize-base64-encoded-image-in-python
+    def __get_resized_image(self):
+        #get the image data for this training image
+        base64_str = self.get_image()
+        #create a buffer to hold image data
+        buffer = io.BytesIO()
+        #decode base64 image back into binary
+        imgdata = b64decode(base64_str)
+        #create a buffer containing binary image data and use this to open an image
+        img = Image.open(io.BytesIO(imgdata))
+        #resize the image
+        new_img = img.resize((100, 100))  # x, y
+        #save the resized image back into the buffer created earlier
+        new_img.save(buffer, format="JPEG")
+        #encode the information in the buffer back into base64
+        img_b64 = b64encode(buffer.getvalue())
+        return img_b64
+
+    #create method to convert image data into numerical data 
+    #code adapted from stckovderflow answer, link below
+    #https://stackoverflow.com/questions/57318892/convert-base64-encoded-image-to-a-numpy-array
+    def __get_numerical_image(self, image_data):
+        #decode the base64 image data back to binary
+        base64_decoded = b64decode(image_data)
+        #open the binary data as an image using PILLOW
+        image = Image.open(io.BytesIO(base64_decoded))
+        #converts the open image to a numpy array and returns it
+        return np.array(image)
 
     def get_source(self):
         return self._source
