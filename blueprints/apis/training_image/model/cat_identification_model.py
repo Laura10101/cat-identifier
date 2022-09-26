25dff7262e498f94c87491ad9733ec912c51521a
Laura Findley <laura@volanto.com>
2022-09-26 15:36:05 +0100

    Define, train, and test the cat identification neural network

--- blueprints/apis/training_image/model/cat_identification_model.py
+++ blueprints/apis/training_image/model/cat_identification_model.py
@@ -1,32 +1,55 @@
-from sklearn.model_selection import train_test_split
 import numpy as np
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Dense, Activation, Flatten, Dropout
+from keras.layers import Conv2D, MaxPooling2D
+from keras import optimizers
+from keras.models import Sequential
 
 class CatIdentificationModel:
     def __init__(self):
+        #Initialise a null model
+        self.__model = None
         #Define the hyperparameters to use during training
         #Number of epochs
-        #Number of batches per epoch
-        #Optimiser
+        self.__epochs = 10
+        #Batch sizes
+        self.__test_batch_size = 64
+        self.__train_batch_size = 64
+        #Learning rate
+        self.__learning_rate = 0.0001
+        #Decay
+        self.__decay = 1e-6
         #Loss function
-        #Testing metrics to use during training
-        pass
+        self.__loss_function = "binary_crossentropy"
+        #Metrics
+        self.__metrics = ["accuracy"]
 
     #create public method to train neural network
     def train_model(self, training_images):
         #this chains the other private methods together 
         #convert test data into numpy arrays so the ML model can use it 
         x_data, y_data = self.__convert_to_numpy(training_images)
-        #standardise the data 
-        x_data, y_data = self.__standardise_data(x_data, y_data)
-        #perform train, test, split
-        x_train, x_test, y_train, y_test = self.__train_test_split(x_data, y_data)
-        #get model
-        self.__model = self.__define_model()
-        #run training loop to do the training 
-        #test model 
-        test_result = self.__test_model(x_test, y_test)
-        #return test results and model file 
-        return test_result
+        #Check to see if either the input or output data is empty
+        if len(x_data) == 0 or len(y_data) == 0:
+            return None #If so, return nothing
+        else:
+            #perform train, test, split
+            x_train, x_test, y_train, y_test = self.__train_test_split(x_data, y_data)
+            #standardise the data 
+            train_iterator, test_iterator = self.__get_data_iterators(x_train, x_test, y_train, y_test)
+            #get model
+            self.__model = self.__define_model(x_train[0].shape, y_train[0].shape[0])
+            #calculate the training and test batch sizes
+            training_batches = train_iterator.n // self.__train_batch_size
+            test_batches = test_iterator.n // self.__test_batch_size
+            #train the model
+            self.__model.fit_generator(generator=train_iterator, steps_per_epoch=training_batches, epochs=self.__epochs)
+            #test model 
+            test_iterator.reset()
+            test_results=self.__model.predict_generator(test_iterator, steps=test_batches, verbose=1)
+            #return test results and model file 
+            return test_results
 
     #create private method to convert training image objects to numpy array 
     def __convert_to_numpy(self, training_images):
@@ -45,18 +68,90 @@ class CatIdentificationModel:
         #return both arrays
         return x_data, y_data
 
-    #create private method to standardise data
-    def __standardise_data(self, training_images, training_labels):
-        pass
+    #create private method to return keras image data iterators for train and test data  
+    #the image data iterators will standardise the image data as required
+    def __get_data_iterators(self, x_train, x_test, y_train, y_test):
+        #Create image data generator to normalize images between 0 and 1
+        data_generator = ImageDataGenerator(rescale=1.0/255.0)
+        #Create our train and test iterators
+        train_iterator = data_generator.flow(x_train, y_train, batch_size=self.__train_batch_size)
+        test_iterator = data_generator.flow(x_test, y_test, batch_size=self.__test_batch_size)
+        return train_iterator, test_iterator
 
     #create private method to perform train/test/split 
     def __train_test_split(self, training_images, training_labels):
         return train_test_split(training_images, training_labels, test_size=0.33, random_state=42)
 
-    #create provate method to define the neural network 
-    def __define_model(self):
-        pass
+    #create private method to define the neural network 
+    def __define_model(self, input_shape, output_shape):
+        #create a sequential model
+        model = Sequential()
+        #create the input layer for the model
+        #the input layer is a convolutional layer
+        #the input shape is 100 x 100 x 3 (width x height x channels)
+        model.add(Conv2D(
+            32, #Number of filters
+            (3,3), #Size of the stride
+            padding='same',
+            input_shape=input_shape
+        ))
+        model.add(Activation('relu'))
 
-       #create private method to test neural network 
-    def __test_model(self, training_images, training_labels):
-        pass
+        #next, create the hidden layers
+        #hidden layer 1 is another convolutional layer
+        #the filters and stride match the input layer
+        model.add(Conv2D(
+            32, #Number of filters
+            (3,3) #Shape of the stride
+        ))
+        model.add(Activation('relu'))
+        #add a MaxPooling layer - this is standard when
+        #working with convolutional neural networks
+        model.add(MaxPooling2D(pool_size=(2,2)))
+        #Randomly deactivate neurons during training to prevent overfitting
+        model.add(Dropout(0.25))
+
+        #hidden layer 2 is also a convolutional layer
+        #this time, the number of filters has been doubled
+        model.add(Conv2D(
+            64, #Number of filters
+            (3,3) #Size of strides
+        ))
+        model.add(Activation('relu'))
+
+        #hidden layer 3 is the final hidden layer (also convolutional)
+        model.add(Conv2D(
+            64, #Number of filters
+            (3,3) #Size of strides
+        ))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2,2)))
+        model.add(Dropout(0.25))
+
+        #create a prediction layer
+        #this is used to extract the most interesting features from
+        #the 3-dimensional data for the purposes of predicting
+        #our 2-dimensional output array
+        #flatten layer converts 3-d to 2-d layer
+        model.add(Flatten())
+        #create a dense layer with 512 neurons/units to make predictions
+        model.add(Dense(512))
+        model.add(Activation('relu'))
+        model.add(Dropout(0.25))
+
+        #create the output layer to predict each label in our target
+        model.add(Dense(
+            output_shape, #One unit per label
+            activation='sigmoid' #Preferred activation function for
+                                 #multi-label image classification
+        ))
+
+        #compile the model
+        model.compile(
+            optimizers.RMSprop(learning_rate=self.__learning_rate, decay=self.__decay),
+            loss=self.__loss_function,
+            metrics=self.__metrics
+        )
+
+        #return the compiled model
+        return model
