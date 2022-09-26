3f7d1a7e2f56ab7d90270baa7115c82bfeb0d030
Laura Findley <laura@volanto.com>
2022-09-26 20:52:53 +0100

    Modify the prediction API layer to produce the prediction and return the label

--- blueprints/apis/prediction/routes.py
+++ blueprints/apis/prediction/routes.py
@@ -16,22 +16,18 @@ def create_prediction():
     try:
         #create an instance of the prediction service
         service  = PredictionService()
-        #get all of the data needed to create a new prediction instance out of the http request
+        #get the image as base64
         image = request.files["image"]
-        is_cat = request.form.get("is_cat") == TRUE
-        colour = request.form.get("colour")
-        is_tabby = request.form.get("is_tabby") == TRUE
-        pattern = request.form.get("pattern")
-        is_pointed = request.form.get("is_pointed") == TRUE
         
         #validate that the file that has been provided is an image file 
         if image.filename == "" or not image or not is_allowed_extension(image.filename):
             raise Exception("A valid .png or .jpg image must be provided when posting a training image")
 
-        #use the service layer to create the prediction and store it in the database, getting the resulting id back
-        prediction_id = service.create_prediction(b64encode(image.read()), is_cat, colour, is_tabby, pattern, is_pointed)
+        #use the service layer to make the prediction and store it in the database
+        #getting the resulting id and predicted label back
+        prediction_id, label = service.create_prediction(b64encode(image.read()))
         #return the created id along with a success code
-        return { "id" : prediction_id }, 200
+        return { "id" : prediction_id, "phenotype": label }, 200
     except Exception as e:
         return { "error": str(e) }, 400
 
