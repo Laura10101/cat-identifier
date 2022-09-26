8bcb98519d3502a1f0d6cfa3b2e207ba84bb04ef
Laura Findley <laura@volanto.com>
2022-09-26 18:56:52 +0100

    Create client class to wrap Predictions API for training API services

---
+++ blueprints/apis/training_image/data/PredictionAPIClient.py
@@ -0,0 +1,19 @@
+from requests import post
+
+#This class provides a wrapper around the Predictions API
+#It is used to post trained models to the prediction API
+#so that the Prediction API can use these for prediction-making
+class PredictionAPIClient:
+    def __init__(self):
+        self.__base_url = "http://127.0.0.1:5000/api/predictions"
+
+    def post_trained_model(self, serialised_model):
+        headers = {
+            "Content-Type": "application/json"
+        }
+        response = post(
+            url=self.__base_url + "/model",
+            headers=headers,
+            json=serialised_model
+        )
+        return response.ok
