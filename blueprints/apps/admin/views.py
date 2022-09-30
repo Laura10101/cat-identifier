d83420c1021f6b7ffcc81d49f0c9692af037947b
Laura Findley <laura@volanto.com>
2022-09-30 18:44:41 +0100

    Declare template folder for the admin app blueprint

--- blueprints/apps/admin/views.py
+++ blueprints/apps/admin/views.py
@@ -3,10 +3,11 @@ from flask import Blueprint, render_template, request, current_app as app
 #Blueprint Configuration
 admin_bp = Blueprint(
     'admin_bp',
-    __name__
+    __name__,
+    template_folder="templates"
 )
 
 #View routes
 @admin_bp.route("/login", methods=["GET"])
 def display_login_form():
-    return render_template("blueprints/apps/admin/templates/login.html")
+    return render_template("login.html")
