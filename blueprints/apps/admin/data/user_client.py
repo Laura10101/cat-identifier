35bbb413af02ef350129589067e3c3bffea7254b
Laura Findley <laura@volanto.com>
2022-09-30 19:56:54 +0100

    Implement user API client to provide user login, auth, and registration functions

--- blueprints/apps/admin/data/user_client.py
+++ blueprints/apps/admin/data/user_client.py
@@ -1,22 +1,45 @@
+from lib2to3.pytree import Base
 from requests import get, post
+from .base_client import BaseAPIClient
 
 #A client class to enable interaction with the user API
-class UserClient:
+class UserClient(BaseAPIClient):
     def __init__(self, base_url):
-        self.__base_url = base_url + "/users/"
-        self.__get_headers = {
+        BaseAPIClient.__init__(self, base_url + "/users")
 
+    def register_user(self, username, password):
+        endpoint = "/"
+        data = {
+            "username": username,
+            "password": password
         }
+        response = self._post(endpoint, data)
+        
+        if response.ok:
+            return None
+        
+        return response.content["error"]
 
-        self.__post_headers = {
-            "Content-Type": "application/json"
+    def authenticate_user(self, username, password):
+        endpoint = "/login"
+        data = {
+            "username": username,
+            "password": password
         }
+        response = self._post(endpoint, data)
 
-    def register_user(self, username, password_hash):
-        return True
+        if response.ok:
+            return response.content["token"]
+        return None
 
-    def user_authenticates(self, username, password_hash):
-        return True
+    def authorise_user(self, username, token):
+        endpoint = "/authorize"
+        data = {
+            "username": username,
+            "token": token
+        }
+        response = self._post(endpoint, data)
 
-    def user_is_authorised(self, token):
-        return True
+        if response.ok:
+            return response.content["token"]
+        return None
