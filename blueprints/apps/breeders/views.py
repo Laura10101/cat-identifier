6a56976d83ecbcd88b4f6f2752e59ab977007bc2
Laura Findley <laura@volanto.com>
2022-10-01 20:13:34 +0100

    Implement skeleton routes and views for breeder app

--- blueprints/apps/breeders/views.py
+++ blueprints/apps/breeders/views.py
@@ -1,8 +1,21 @@
-from flask import Blueprint, request, current_app as app
+from flask import Blueprint, render_template, request, current_app as app
 
 #Blueprint Configuration
 breeders_bp = Blueprint(
     'breeders_bp',
     __name__,
-    template_folder="templates"
+    template_folder="templates",
+    static_folder="static"
 )
+
+@breeders_bp.route("/", methods=["GET"])
+def upload_image():
+    return render_template("upload-cat-image.html")
+
+@breeders_bp.route("/results", methods=["POST"])
+def get_results():
+    return render_template("results.html")
+
+@breeders_bp.route("/thankyou", methods=["POST"])
+def thankyou():
+    return render_template("thankyou.html")
