e232c1ecac4d199baeaaa036513ed7f1384aac31
Laura Findley <laura@volanto.com>
2022-10-01 20:12:53 +0100

    Create main.js for breeder app with skeleton functions

---
+++ blueprints/apps/breeders/static/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    //Initialise modal dialogues
+    $('.modal').modal();
+});
+
+function getCatPhenotype(image) {
+
+}
+
+function submitUserFeedback(accepted) {
+    
+}
+
+function httpPost(endpoint, data) {
+
+}
