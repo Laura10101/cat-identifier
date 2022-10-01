1d506d73b5ce444cd86ca0dc15444f999bbbc5e5
Laura Findley <laura@volanto.com>
2022-10-01 20:28:07 +0100

    Add skeleton functions to main.js for admin app

--- blueprints/apps/admin/static/js/main.js
+++ blueprints/apps/admin/static/js/main.js
@@ -2,3 +2,19 @@ $(document).ready(function() {
     //Initialise the dropdown elements of the navbar
     $(".dropdown-trigger").dropdown({ hover: true });
 })
+
+function uploadTrainingImages(zipFile, modalInstance) {
+
+}
+
+function importTrainingImages(zipFile, modalInstance) {
+
+}
+
+function labelTrainingImages(zipFile, modalInstance) {
+
+}
+
+function updateTrainingStatus() {
+
+}
