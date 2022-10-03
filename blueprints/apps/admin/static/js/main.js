13759959bbc7b7e9d327595703afcf3bb3352ccf
Laura Findley <laura@volanto.com>
2022-10-03 16:52:00 +0100

    Implement functions for managing modals and HTTP requests in admin app

--- blueprints/apps/admin/static/js/main.js
+++ blueprints/apps/admin/static/js/main.js
@@ -2,11 +2,12 @@ function initMaterialize() {
     //Initialise the dropdown elements of the navbar
     $(".dropdown-trigger").dropdown({ hover: true });
 
+    //Initialise the modal plugin
     var elems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(elems, options);
 }
 
-function uploadTrainingImages(zipFile, modalInstance) {
+function uploadTrainingImages(zipFile) {
 
 }
 
@@ -14,7 +15,7 @@ function confirmUpload() {
 
 }
 
-function importTrainingImages(zipFile, modalInstance) {
+function importTrainingImages(zipFile) {
 
 }
 
@@ -22,7 +23,7 @@ function confirmImport() {
 
 }
 
-function labelTrainingImages(zipFile, modalInstance) {
+function labelTrainingImages(zipFile) {
 
 }
 
@@ -34,10 +35,39 @@ function updateTrainingStatus() {
 
 }
 
-function httpPost(endpoint, json, callback) {
-
+//Use JQuery to make HTTP post requests to the APIs
+//Taken from StackOverflow: https://stackoverflow.com/questions/6323338/jquery-ajax-posting-json-to-webservice
+function httpPost(endpoint, data, success, error) {
+    $.ajax({
+        type: "POST",
+        url: endpoint,
+        data: JSON.stringify(data),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: success,
+        error: error
+    });
 }
 
-function httpGet(endpoint, callback) {
+//Use JQuery to make HTTP get requests to the APIs
+//Adapted from the httpPost function above
+function httpGet(endpoint, success, error) {
+    $.ajax({
+        type: "GET",
+        url: endpoint,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: success,
+        error: error
+    });
+}
 
+//function to open a Materialize modal based on its ID
+function showModal(modalId) {
+    M.Modal.getInstance(document.getElementById(modalId)).open();
+}
+
+//function to close a Materialize modal based on its ID
+function closeModal(modalId) {
+    M.Modal.getInstance(document.getElementById(modalId)).close();
 }
