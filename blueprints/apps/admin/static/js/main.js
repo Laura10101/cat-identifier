187bb9a07a42536f032f331127d79932a239f1a5
Laura Findley <laura@volanto.com>
2022-10-03 22:01:39 +0100

    Fix bug when initiailising Materialize in admin app

--- blueprints/apps/admin/static/js/main.js
+++ blueprints/apps/admin/static/js/main.js
@@ -7,7 +7,7 @@ function initMaterialize() {
 
     //Initialise the modal plugin
     var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, options);
+    var instances = M.Modal.init(elems);
 }
 
 function uploadTrainingImages(b64ZipFile) {
