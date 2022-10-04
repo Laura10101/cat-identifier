d290d075de1ec702a55f95fd37c51b8c28083c73
Laura Findley <laura@volanto.com>
2022-10-04 21:50:11 +0100

    Implement celery train_model task

---
+++ blueprints/apis/training_image/tasks/tasks.py
@@ -0,0 +1,19 @@
+from celery import shared_task
+from datetime import datetime
+from ..services import TrainingImageService
+from ..model import TrainingLogEntry
+from ..data import TrainingLogRepository
+
+@shared_task(name="training_tasks.train_model")
+def train_model():
+    service = TrainingImageService()
+    repo = TrainingLogRepository()
+    try:
+        #clear log before commmencing training
+        repo.clear_log()
+        service.train_new_model()
+    except Exception as e:
+        #if an exception occurs, create a log entry
+        timestamp = datetime.now()
+        message = str(e)
+        repo.update_log(TrainingLogEntry(timestamp, message))
