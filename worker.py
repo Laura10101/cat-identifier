c07841fc191d20837be2baf9ea0e3efcf3e43b9a
Laura Findley <laura@volanto.com>
2022-10-04 23:05:33 +0100

    Implement celery worker script based on SO answer and Flask documentation

---
+++ worker.py
@@ -0,0 +1,17 @@
+from celery import Celery
+import sys, os
+sys.dont_write_bytecode = True
+
+#import the env file if it exists
+if os.path.exists("env.py"):
+    import env
+
+#Inspired by StackOverflow: https://stackoverflow.com/questions/22172915/relative-imports-require-the-package-argument
+def make_celery_worker(broker_url):
+    return Celery(
+        "flask-celery-app",
+        broker=broker_url,
+        include=["blueprints.apis.training_image.tasks"]
+    )
+
+worker = make_celery_worker(os.environ.get("BROKER_URL"))
