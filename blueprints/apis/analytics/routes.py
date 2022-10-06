62a1a887801a0036b21ea3c23a172ac7cb59f023
Laura Findley <laura@volanto.com>
2022-10-06 21:55:50 +0100

    Refactor analytics API to avoid circular references

--- blueprints/apis/analytics/routes.py
+++ blueprints/apis/analytics/routes.py
@@ -1,5 +1,6 @@
 import traceback
 from flask import Blueprint, request, current_app as app
+from flask_sqlalchemy import SQLAlchemy
 from .services import AnalyticsService
 
 #Blueprint Configuration
@@ -15,8 +16,24 @@ def make_service():
 
 #global user service instance
 service = make_service()
+db = SQLAlchemy(app)
 
 #Ping endpoint used to test connections to the API
 @analytics_bp.route('/ping', methods=["GET"])
 def ping():
+    return {}, 200
+
+#Create endpoint to post a snapshot of training image data
+@analytics_bp.route('/snapshots/training-images')
+def post_training_image_snapshot():
+    return {}, 200
+
+#Create endpoint to post a snapshot of prediction data
+@analytics_bp.route('/snapshots/predictions')
+def post_predictions_snapshot():
+    return {}, 200
+
+#Create endpoint to post a snapshot of prediction model data
+@analytics_bp.route('/snapshots/prediction-models')
+def post_prediction_models_snapshot():
     return {}, 200
