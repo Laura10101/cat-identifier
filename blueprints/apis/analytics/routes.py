ec504131330f7103c2649be595d72388f6492cc0
Laura Findley <laura@volanto.com>
2022-10-12 22:32:12 +0100

    Implement routing methods for creating snapshots in analytics API

--- blueprints/apis/analytics/routes.py
+++ blueprints/apis/analytics/routes.py
@@ -24,14 +24,126 @@ def ping():
 #Create endpoint to post a snapshot of training image data
 @analytics_bp.route('/snapshots/training-images')
 def post_training_image_snapshot():
-    return {}, 200
+    try:
+        # one snapshot may comprise several rows as each
+        # row in a fact table represents a single permutation of
+        # the dimensions in that table
+        summaries = request.json["summaries"]
+        for summary in summaries:
+            validate_label(**summary)
+
+            if "source" not in summary:
+                raise Exception("Missing training image source from training image snapshot")
+
+            if "count" not in summary:
+                raise Exception("Missing count from training image snapshot")
+
+            # create the snapshot
+            service.create_training_images_snapshot(
+                summary["is_unlabelled"],
+                summary["is_cat"],
+                summary["colour"],
+                summary["is_tabby"],
+                summary["pattern"],
+                summary["is_pointed"],
+                summary["source"],
+                summary["count"]
+            )
+
+        return {}, 201
+    except Exception as e:
+        return { "error": str(e) }, 400
 
 #Create endpoint to post a snapshot of prediction data
 @analytics_bp.route('/snapshots/predictions')
 def post_predictions_snapshot():
-    return {}, 200
+    try:
+        summaries = request.json["summaries"]
+        for summary in summaries:
+            validate_label(**summary)
+
+            if not "user_review_status" in summary:
+                raise Exception("Attribute user_review_status is missing from summary")
+
+            if not "admin_review_status" in summary:
+                raise Exception("Attribute admin_review_status is missing from summary")
+
+            if "count" not in summary:
+                raise Exception("Missing count from training image snapshot")
+            
+            service.create_predictions_snapshot(
+                summary["is_unlabelled"],
+                summary["is_cat"],
+                summary["colour"],
+                summary["is_tabby"],
+                summary["pattern"],
+                summary["is_pointed"],
+                summary["user_review_status"],
+                summary["admin_review_status"],
+                summary["count"]
+            )
+
+        return {}, 201
+    except Exception as e:
+        return { "error": str(e) }, 400
 
 #Create endpoint to post a snapshot of prediction model data
 @analytics_bp.route('/snapshots/prediction-models')
 def post_prediction_models_snapshot():
-    return {}, 200
+    try:
+        expected_args = [
+                "training_started", "training_ended", "min_accuracy", "max_accuracy", "avg_accuracy",
+                "min_loss", "max_loss", "avg_loss"
+            ]
+        summaries = request.json["summaries"]
+        for summary in summaries:
+
+            # check that all args are present
+            for arg in expected_args:
+                if not arg in summary:
+                    raise Exception("Missing " + arg + " in model snapshot summary")
+
+            service.create_models_snapshot(
+                summary["training_started"],
+                summary["training_ended"],
+                summary["min_accuracy"],
+                summary["max_accuracy"],
+                summary["avg_accuracy"],
+                summary["min_loss"],
+                summary["max_loss"],
+                summary["avg_loss"]
+            )
+
+        return {}, 201
+    except Exception as e:
+        return { "error": str(e) }, 400
+
+# create endpoint to get size of training set by date
+
+# create endpoint to get breakdown of training images by label
+
+# create endpoint to get historical model performance over time
+
+# create endpoint to get breakdown of prediction accuracy over time
+
+# create endpoint to get breakdown of predictions by review status and date
+
+# create endpoint to get breakdown of admin versus user acceptance of predictions
+
+### HELPER FUNCTIONS ###
+def validate_label(**kwargs):
+    expected_args = ["is_unlabelled", "is_cat", "colour", "is_tabby", "pattern", "is_pointed"]
+
+    # all expected args should be present
+    for arg in expected_args:
+        if not arg in kwargs:
+            raise Exception("Label is missing attribute: " + arg)
+
+    # if a cat is unlabelled, then all other args should be null
+    if kwargs["is_unlabelled"]:
+        for arg in expected_args[1:]:
+            if not kwargs[arg] is None:
+                raise Exception("Unlabelled image cannot have value for " + arg)
+
+    # if no exception has been thrown yet, validation has passed
+    return True
